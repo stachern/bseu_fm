fb4710ae76b8e923c0205bdd09d646c165763913
stachern <stas.cherniakov@gmail.com>
2013-03-20 02:45:25 +0300

    load feed example

---
+++ media/js/newsfeed.js
@@ -0,0 +1,24 @@
+var FeedServiceUrl = 'http://2.bseu-fm.appspot.com/feed';
+var FeedOffset = 0;
+
+function display_loaded_articles(data){
+    $('.progress').addClass('hidden');
+    var rendered_articles = new EJS({'url':'/js/templates/article.ejs'}).render({articles: data});
+    $('#feed').append(rendered_articles);
+}
+
+function get_feed(offset){
+    $('.progress').removeClass('hidden');
+    $.getJSON(FeedServiceUrl + '?offset='+offset, display_loaded_articles);
+}
+
+$(document).ready(function(){
+    get_feed(FeedOffset);
+});
+
+$(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+       FeedOffset += 10;
+       get_feed(FeedOffset);
+    }
+});
