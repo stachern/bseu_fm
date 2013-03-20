25fefcb725d59b122f6b3cd9fe2bfc1372eb721d
Egor Zhuk <egzhuk@gmail.com>
2013-03-20 17:16:46 +0300

    Some css improvements + news feed

--- media/js/custom.js
+++ media/js/custom.js
@@ -10,3 +10,20 @@ _gaq.push(['_trackPageview']);
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(ga, s);
 })();
+
+
+$('#rssdata').ready(function()
+{
+    var pipe_url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=3f5db5135e0d956c2ef490cd1ae22878&_render=json&_callback=?';
+    $.getJSON(pipe_url,function(data)
+    {
+      
+      $(data.value.items).each(function(index,item)
+        {
+            var item_html = '<li><a href="'+item.link+'">'+item.title+'</a></li>';
+            $('#rssdata ul.rss-items').append(item_html);
+        });
+        $('#rssdata div.loading').fadeOut();
+        $('#rssdata ul.rss-items').slideDown();
+    });
+});
