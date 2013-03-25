1ca3d2477e2fa230a92af5e3df2aaa34871f9ed2
Stanislau Charniakou <stanislau_charniakou@epam.com>
2013-03-25 17:53:44 +0300

    a bit of perfectionism :)

--- media/js/newsfeed.js
+++ media/js/newsfeed.js
@@ -30,34 +30,19 @@ $(window).scroll(function() {
     }
 });
 
-$('#rssdata').ready(function()
-{
-    var pipe_url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=3f5db5135e0d956c2ef490cd1ae22878&_render=json&_callback=?';
-    $.getJSON(pipe_url,function(data)
-    {
-      
-      $(data.value.items).each(function(index,item)
-        {
-            var item_html = '<li><a href="'+item.link+'">'+item.title+'</a></li>';
-            $('#rssdata ul.rss-items').append(item_html);
-        });
+$('#rssdata').ready(function(){
+    $.getJSON('http://pipes.yahoo.com/pipes/pipe.run?_id=3f5db5135e0d956c2ef490cd1ae22878&_render=json&_callback=?' ,function(data){
+        var rendered_html =  new EJS({'url':'/js/templates/news.ejs'}).render({news: data.value.items});
+        $('#rssdata ul.rss-items').append(rendered_html);
         $('#rssdata div.loading').fadeOut();
         $('#rssdata ul.rss-items').slideDown();
     });
 });
 
-$('#twitter').ready(function()
-{
-    var pipe_url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=ca64fc1563fcf96ef51ec38da7ca3cfc&_render=json&_callback=?';
-    $.getJSON(pipe_url,function(data)
-    {
-      
-      $(data.value.items).each(function(index,item)
-        {
-            var item_id=item.link.substring(35);           
-            var item_html = '<li><p class="pull-right"><a href="https://twitter.com/intent/tweet?in_reply_to='+item_id+'"><img src="/img/logos/twitter/reply.png"></a>&nbsp;<a href="https://twitter.com/intent/retweet?tweet_id='+item_id+'"><img src="/img/logos/twitter/rt.png"></a>&nbsp;<a href="https://twitter.com/intent/favorite?tweet_id='+item_id+'"><img src="/img/logos/twitter/fv.png"></a>&nbsp;</p><p><a class="tw" href="'+item.link+'">'+item.title.substring(8)+'</a></p></li>';
-            $('#twitter ul.twits').append(item_html);
-        });
+$('#twitter').ready(function(){
+    $.getJSON('http://pipes.yahoo.com/pipes/pipe.run?_id=ca64fc1563fcf96ef51ec38da7ca3cfc&_render=json&_callback=?', function(data){
+        var rendered_html =  new EJS({'url':'/js/templates/twit.ejs'}).render({twits: data.value.items});
+        $('#twitter ul.twits').append(rendered_html);
         $('#twitter div.loading').fadeOut();
         $('#twitter ul.twits').slideDown();
     });
