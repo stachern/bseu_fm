e83e1c373167ed438bafbb34a505ea07123552fc
Dzianis Dashkevich <dskecse@gmail.com>
2015-04-21 18:11:07 +0300

    Display proper message for outdated browsers

---
+++ media/js/outdated/check.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  outdatedBrowser({
+    bgColor: '#f25648',
+    color: '#ffffff',
+    lowerThan: 'transform',
+    languagePath: SUBDIR_PREFIX + '/js/outdated/lang/ru.html'
+  })
+});
