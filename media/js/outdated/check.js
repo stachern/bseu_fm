5cdae1880988193d2d8939e93c4133480b847d8e
Dzianis Dashkevich <dskecse@gmail.com>
2016-08-07 19:31:27 +0300

    Remove outdated browsers check

--- media/js/outdated/check.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  outdatedBrowser({
-    bgColor: '#f25648',
-    color: '#ffffff',
-    lowerThan: 'transform',
-    languagePath: SUBDIR_PREFIX + '/js/outdated/lang/ru.html'
-  })
-});
